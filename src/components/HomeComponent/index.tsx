eb66142e4cde0fd345ebf6bbd4269a1a23b379fc
Ryan Barclay <mrryanbarclay@gmail.com>
2022-07-02 16:18:25 -0700

    end of day update

--- src/components/HomeComponent/index.tsx
+++ src/components/HomeComponent/index.tsx
@@ -1,19 +1,29 @@
 import { Paper } from "@mui/material";
 import styled from "@emotion/styled";
+import { makeNoise2D } from "fast-simplex-noise";
 
 const StyledPaper = styled(Paper)`
   height: 200vh;
 `;
 
-// const lerp = (a0: number, a1: number, w: number): number => {
-//   assert(w >= 0 && w <= 0);
-//   return (1.0 - w) * a0 + w * a1;
-// };
+const mountian = (): JSX.Element => {
+  const noise = makeNoise2D();
+  const linePoints = (): string => {
+    let stringOutput = "";
+    for (let index = 400; index > 0; index--) {
+      stringOutput = stringOutput.concat(
+        "" + index + "," + noise(index, 0) * 50 + " "
+      );
+    }
+    return stringOutput;
+  };
+  return <polygon points={linePoints()} />;
+};
 
 const HomeComponent = (): JSX.Element => {
   return (
     <StyledPaper>
-      <svg></svg>
+      <svg style={{ height: "1000em", width: "1000em" }}>{mountian()}</svg>
     </StyledPaper>
   );
 };
