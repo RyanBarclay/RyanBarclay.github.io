3220eb992f3da84910df7aa5e750dfbc233fa25c
RyanBarclay <mrryanbarclay@gmail.com>
2023-03-13 15:54:40 -0700

    new vite boilerplate

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,34 @@
-import React, { useContext } from "react";
-import { ThemeProvider } from "@mui/material/styles";
-import { darkTheme, lightTheme } from "./theme/theme";
-import { Box, CssBaseline } from "@mui/material";
-import { ThemeContext } from "./Context/DarkModeContext";
-import LeftSide from "./Components/Molecule/SideBar/LeftSide";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
 
 function App() {
-  const { isDarkTheme } = useContext(ThemeContext);
+  const [count, setCount] = useState(0)
+
   return (
-    <ThemeProvider theme={isDarkTheme ? darkTheme : lightTheme}>
-      <CssBaseline />
-      <Box sx={{ display: "flex", gap: "10%" }}>
-        <LeftSide />
-        {/* <Content /> */}
-      </Box>
-    </ThemeProvider>
-  );
+    <div className="App">
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  )
 }
 
-export default App;
+export default App
